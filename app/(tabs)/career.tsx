77c8e7038575abd4f91688315da964b4fdcb7469
Yaw Adomako <adomakoy42@gmail.com>
2025-06-21 06:43:42 -0400

    fixed the pharmacy error

--- app/(tabs)/career.tsx
+++ app/(tabs)/career.tsx
@@ -198,7 +198,7 @@ const careers = [
     description: 'Dispense medications and provide expertise on drug use.',
     likes: ['Medications', 'Patient Safety', 'Consultation'],
     category: 'Healthcare',
-    icon: 'pharmacy',
+    icon: 'medical-bag', // or 'pill'
   },
   {
     title: 'Medical Laboratory Scientist',
